beea2f383196095d9c77a0f36eb833064eda9a11
Whilmar Bitoco <whlmrbitoco@gmail.com>
2026-02-10 12:32:13 +0800

    added important charts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
+<script>
+	import BarChart from '$lib/components/BarChart.svelte';
+	import AreaChart from '$lib/components/AreaChart.svelte';
+	import LineChart from '$lib/components/LineChart.svelte';
+	import PieChart from '$lib/components/PieChart.svelte';
+	import RadarChart from '$lib/components/RadarChart.svelte';
+	import RadialChart from '$lib/components/RadialChart.svelte';
 </script>
 
-<div>
-	<Button onclick={() => alert('Button clicked!')}>Click me</Button>
+<div class="space-y-8 p-5 sm:p-10">
+	<BarChart />
+	<AreaChart />
+	<LineChart />
+
+	<div class="grid grid-cols-1 gap-8 md:grid-cols-3">
+		<PieChart />
+		<RadarChart />
+		<RadialChart />
+	</div>
 </div>
